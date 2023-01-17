02fecd545eeee1efcfd5548251d9f93c5135a48b
Benjamin Faller <git@pwned.info>
2023-01-17 16:50:15 +0100

    Latest working version

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use simconnect_sdk::{FlxClientEvent, Notification, SimConnect, SimConnectObject}
 use std::io::Write;
 use std::{
     io::{BufRead, BufReader},
-    sync::{atomic::AtomicBool, mpsc},
+    sync::mpsc,
     time::Duration,
 };
 
@@ -13,9 +13,8 @@ const BAUD_RATE: u32 = 115200;
 /// A data structure that will be used to receive data from SimConnect.
 /// See the documentation of `SimConnectObject` for more information on the arguments of the `simconnect` attribute.
 #[derive(Debug, Clone, SimConnectObject)]
-#[simconnect(period = "sim-frame", condition = "changed", interval = 50)]
-#[allow(dead_code)]
-struct AirplaneData {
+#[simconnect(period = "sim-frame", condition = "changed")]
+struct AirplaneSimData {
     #[simconnect(name = "GEAR CENTER POSITION", unit = "percent over 100")]
     gear_center_position: f64,
     #[simconnect(name = "GEAR LEFT POSITION", unit = "percent over 100")]
@@ -25,32 +24,33 @@ struct AirplaneData {
 
     /// Parking brake indicator.
     ///
-    /// WARNING: Must be the last entry in the struct due to a bug in the `simconnect-sdk` crate, otherwise the gear position values are interpreted false.
+    /// WARNING: Must be the last entry in the struct due to a bug in the `simconnect-sdk` crate, otherwise the gear
+    /// position values are interpreted incorrectly.
     #[simconnect(name = "BRAKE PARKING INDICATOR")]
     parking_brake_indicator: bool,
 }
 
-#[derive(Debug)]
-struct SimStatus {
+#[derive(Debug, PartialEq, Eq)]
+struct AircraftSimState {
     parking_brake_indicator: bool,
     gear_center_state: LandingGearStatus,
     gear_left_state: LandingGearStatus,
     gear_right_state: LandingGearStatus,
 }
 
-impl From<AirplaneData> for SimStatus {
-    fn from(value: AirplaneData) -> Self {
+impl From<AirplaneSimData> for AircraftSimState {
+    fn from(value: AirplaneSimData) -> Self {
         Self {
             parking_brake_indicator: value.parking_brake_indicator,
             gear_center_state: value.gear_center_position.into(),
             gear_left_state: value.gear_left_position.into(),
-            gear_right_state: value.gear_left_position.into(),
+            gear_right_state: value.gear_right_position.into(),
         }
     }
 }
 
-impl SimStatus {
-    fn send(&self, tx: &mut Box<dyn SerialPort>) -> Result<(), std::io::Error> {
+impl AircraftSimState {
+    fn send_state(&self, tx: &mut Box<dyn SerialPort>) -> Result<(), std::io::Error> {
         writeln!(tx, "PARKING_BRAKE:{}", self.parking_brake_indicator as i32)?;
         writeln!(tx, "FRONT_GEAR_LED:{}", self.gear_center_state.as_int())?;
         writeln!(tx, "LEFT_GEAR_LED:{}", self.gear_left_state.as_int())?;
@@ -59,7 +59,7 @@ impl SimStatus {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 enum LandingGearStatus {
     Unknown,
     Up,
@@ -92,10 +92,12 @@ impl LandingGearStatus {
 enum Event {
     Keepalive,
     SynAck,
-    /// Reset the connection to the Arduino
-    Reset,
-    Command(String),
-    Sim(SimStatus),
+    /// Reset the connection to the Arduino.
+    Disconnect,
+    /// The hardware state of the panel was changed.
+    PanelChange(String),
+    /// The simulator state changed.
+    SimChange(AircraftSimState),
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -159,27 +161,30 @@ fn run_simconnect(event_tx: mpsc::Sender<Event>) {
 
         match notification {
             Some(Notification::Open) => {
-                println!("Connection opened.");
+                println!("Connected to flight simulator via SimConnect");
                 // After the connection is successfully open, we register the struct
-                client.register_object::<AirplaneData>().unwrap();
+                client.register_object::<AirplaneSimData>().unwrap();
             }
             Some(Notification::Object(data)) => {
+                let airplane_state = AirplaneSimData::try_from(&data).unwrap();
+                dbg!(&airplane_state);
                 event_tx
-                    .send(Event::Sim(AirplaneData::try_from(&data).unwrap().into()))
+                    .send(Event::SimChange(airplane_state.into()))
                     .unwrap();
             }
             _ => (),
         }
 
-        // sleep for about a frame to reduce CPU usage
+        // Sleep for about a frame to reduce CPU usage
         std::thread::sleep(std::time::Duration::from_millis(10));
     }
 }
 
 fn run_keepalive(tx: mpsc::Sender<Event>) {
     loop {
+        tx.send(Event::Keepalive)
+            .expect("Failed to send keepalive event");
         std::thread::sleep(Duration::from_millis(500));
-        tx.send(Event::Keepalive).unwrap();
     }
 }
 
@@ -189,8 +194,8 @@ fn run_serial_reader(event_tx: mpsc::Sender<Event>, serial_rx: Box<dyn SerialPor
         match line {
             Ok(command) => match command.as_str() {
                 "SYN|ACK" => event_tx.send(Event::SynAck).unwrap(),
-                "RST" => event_tx.send(Event::Reset).unwrap(),
-                _ => event_tx.send(Event::Command(command)).unwrap(),
+                "RST" => event_tx.send(Event::Disconnect).unwrap(),
+                _ => event_tx.send(Event::PanelChange(command)).unwrap(),
             },
             Err(e) => {
                 if e.kind() != std::io::ErrorKind::TimedOut {
@@ -214,7 +219,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     let port_path = args.nth(1).unwrap();
 
-    let mut connected = AtomicBool::new(false);
+    let mut connected = false;
 
     let (event_tx, event_rx) = std::sync::mpsc::channel();
 
@@ -257,17 +262,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     client.map_client_event_to_sim_event(SimClientEvent::LandingGearUp)?;
     client.map_client_event_to_sim_event(SimClientEvent::LandingGearDown)?;
 
+    let mut aircraft_sim_state = Option::None;
+
     for event in event_rx {
         println!("Received event {:?}", &event);
         match event {
-            Event::Keepalive if *connected.get_mut() => writeln!(port_tx, "PING")?,
+            Event::Keepalive if connected => writeln!(port_tx, "PING")?,
             Event::SynAck => {
                 writeln!(port_tx, "ACK")?;
-                *connected.get_mut() = true;
+                connected = true;
             }
-            Event::Reset => *connected.get_mut() = false,
-            Event::Sim(simstate) => simstate.send(&mut port_tx)?,
-            Event::Command(cmd) => match cmd.as_str() {
+            Event::Disconnect => connected = false,
+            Event::SimChange(state) => {
+                if aircraft_sim_state
+                    .map(|old_state| old_state != state)
+                    .unwrap_or_default()
+                {
+                    state.send_state(&mut port_tx)?;
+                }
+                aircraft_sim_state = Some(state);
+            }
+            Event::PanelChange(cmd) => match cmd.as_str() {
                 "MISC1:0" => client.transmit_event(SimClientEvent::TaxiLightsOff)?,
                 "MISC1:1" => client.transmit_event(SimClientEvent::TaxiLightsOn)?,
                 "MISC2:0" => client.transmit_event(SimClientEvent::LandingLightsOff)?,
